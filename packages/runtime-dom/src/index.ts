f60fa60a79560a4f41722c968f8b9f857c964e11
panghu <931161563@qq.com>
2023-03-21 22:16:02 +0800

    init

---
+++ packages/runtime-dom/src/index.ts
@@ -0,0 +1,265 @@
+import {
+  createRenderer,
+  createHydrationRenderer,
+  warn,
+  RootRenderFunction,
+  CreateAppFunction,
+  Renderer,
+  HydrationRenderer,
+  App,
+  RootHydrateFunction,
+  isRuntimeOnly,
+  DeprecationTypes,
+  compatUtils
+} from '@vue/runtime-core'
+import { nodeOps } from './nodeOps'
+import { patchProp } from './patchProp'
+// Importing from the compiler, will be tree-shaken in prod
+import {
+  isFunction,
+  isString,
+  isHTMLTag,
+  isSVGTag,
+  extend,
+  NOOP
+} from '@vue/shared'
+
+declare module '@vue/reactivity' {
+  export interface RefUnwrapBailTypes {
+    runtimeDOMBailTypes: Node | Window
+  }
+}
+
+// 渲染相關的一些配置,比如更新屬性的方法,操作DOM的方法
+const rendererOptions = /*#__PURE__*/ extend({ patchProp }, nodeOps)
+
+// lazy create the renderer - this makes core renderer logic tree-shakable
+// in case the user only imports reactivity utilities from Vue.
+// 延時創建渲染器，當用戶只依赖响应式包的时候，可以通过tree-shaking移除和兴渲染逻辑相关的代码
+let renderer: Renderer<Element | ShadowRoot> | HydrationRenderer
+
+let enabledHydration = false
+//ensureRenderer()用来创建一个渲染器对象，内部代码
+function ensureRenderer() {
+  return (
+    renderer ||
+    (renderer = createRenderer<Node, Element | ShadowRoot>(rendererOptions))
+  )
+}
+
+function ensureHydrationRenderer() {
+  renderer = enabledHydration
+    ? renderer
+    : createHydrationRenderer(rendererOptions)
+  enabledHydration = true
+  return renderer as HydrationRenderer
+}
+
+// use explicit type casts here to avoid import() calls in rolled-up d.ts
+export const render = ((...args) => {
+  ensureRenderer().render(...args)
+}) as RootRenderFunction<Element | ShadowRoot>
+
+export const hydrate = ((...args) => {
+  ensureHydrationRenderer().hydrate(...args)
+}) as RootHydrateFunction
+
+export const createApp = ((...args) => {
+  // 創建app對象
+  const app = ensureRenderer().createApp(...args)
+
+  if (__DEV__) {
+    injectNativeTagCheck(app)
+    injectCompilerOptionsCheck(app)
+  }
+  const { mount } = app
+  // 为什么要重写mount方法？不放在mount方法内部实现呢？
+  // 因为vue.js不仅仅是为web平台服务，它的目标是支持跨平台渲染
+  // createApp函数内部的app.mount方法是一个标准的可跨平台的组件渲染流程----先创建vnode再渲染vnode，
+  // rootcontainer可以是web平台的dom对象，在其他平台 weex或者小程序中可以使其他对象
+  // 既能让用户在使用API时可以更加灵活，也兼容了vue2的写法
+  // 比如app.mount的第一个参数就同时支持选择器字符串和DOM对象两种写法
+  app.mount = (containerOrSelector: Element | ShadowRoot | string): any => {
+    // 标准化容器
+    const container = normalizeContainer(containerOrSelector)
+    if (!container) return
+
+    const component = app._component
+    // 如果组件对象没用定义render函数和template模板，则取容器的innerHTML作为组件模板内容
+    if (!isFunction(component) && !component.render && !component.template) {
+      // __UNSAFE__
+      // Reason: potential execution of JS expressions in in-DOM template.
+      // The user must make sure the in-DOM template is trusted. If it's
+      // rendered by the server, the template should not contain any user data.
+      component.template = container.innerHTML
+      // 2.x compat check
+      if (__COMPAT__ && __DEV__) {
+        for (let i = 0; i < container.attributes.length; i++) {
+          const attr = container.attributes[i]
+          if (attr.name !== 'v-cloak' && /^(v-|:|@)/.test(attr.name)) {
+            compatUtils.warnDeprecation(
+              DeprecationTypes.GLOBAL_MOUNT_CONTAINER,
+              null
+            )
+            break
+          }
+        }
+      }
+    }
+
+    // clear content before mounting
+    container.innerHTML = ''
+    // 真正的挂载
+    const proxy = mount(container, false, container instanceof SVGElement)
+    if (container instanceof Element) {
+      container.removeAttribute('v-cloak')
+      container.setAttribute('data-v-app', '')
+    }
+    return proxy
+  }
+
+  return app
+}) as CreateAppFunction<Element>
+
+export const createSSRApp = ((...args) => {
+  const app = ensureHydrationRenderer().createApp(...args)
+
+  if (__DEV__) {
+    injectNativeTagCheck(app)
+    injectCompilerOptionsCheck(app)
+  }
+
+  const { mount } = app
+  app.mount = (containerOrSelector: Element | ShadowRoot | string): any => {
+    const container = normalizeContainer(containerOrSelector)
+    if (container) {
+      return mount(container, true, container instanceof SVGElement)
+    }
+  }
+
+  return app
+}) as CreateAppFunction<Element>
+
+function injectNativeTagCheck(app: App) {
+  // Inject `isNativeTag`
+  // this is used for component name validation (dev only)
+  Object.defineProperty(app.config, 'isNativeTag', {
+    value: (tag: string) => isHTMLTag(tag) || isSVGTag(tag),
+    writable: false
+  })
+}
+
+// dev only
+function injectCompilerOptionsCheck(app: App) {
+  if (isRuntimeOnly()) {
+    const isCustomElement = app.config.isCustomElement
+    Object.defineProperty(app.config, 'isCustomElement', {
+      get() {
+        return isCustomElement
+      },
+      set() {
+        warn(
+          `The \`isCustomElement\` config option is deprecated. Use ` +
+          `\`compilerOptions.isCustomElement\` instead.`
+        )
+      }
+    })
+
+    const compilerOptions = app.config.compilerOptions
+    const msg =
+      `The \`compilerOptions\` config option is only respected when using ` +
+      `a build of Vue.js that includes the runtime compiler (aka "full build"). ` +
+      `Since you are using the runtime-only build, \`compilerOptions\` ` +
+      `must be passed to \`@vue/compiler-dom\` in the build setup instead.\n` +
+      `- For vue-loader: pass it via vue-loader's \`compilerOptions\` loader option.\n` +
+      `- For vue-cli: see https://cli.vuejs.org/guide/webpack.html#modifying-options-of-a-loader\n` +
+      `- For vite: pass it via @vitejs/plugin-vue options. See https://github.com/vitejs/vite/tree/main/packages/plugin-vue#example-for-passing-options-to-vuecompiler-dom`
+
+    Object.defineProperty(app.config, 'compilerOptions', {
+      get() {
+        warn(msg)
+        return compilerOptions
+      },
+      set() {
+        warn(msg)
+      }
+    })
+  }
+}
+
+function normalizeContainer(
+  container: Element | ShadowRoot | string
+): Element | null {
+  if (isString(container)) {
+    const res = document.querySelector(container)
+    if (__DEV__ && !res) {
+      warn(
+        `Failed to mount app: mount target selector "${container}" returned null.`
+      )
+    }
+    return res
+  }
+  if (
+    __DEV__ &&
+    window.ShadowRoot &&
+    container instanceof window.ShadowRoot &&
+    container.mode === 'closed'
+  ) {
+    warn(
+      `mounting on a ShadowRoot with \`{mode: "closed"}\` may lead to unpredictable bugs`
+    )
+  }
+  return container as any
+}
+
+// Custom element support
+export {
+  defineCustomElement,
+  defineSSRCustomElement,
+  VueElement,
+  type VueElementConstructor
+} from './apiCustomElement'
+
+// SFC CSS utilities
+export { useCssModule } from './helpers/useCssModule'
+export { useCssVars } from './helpers/useCssVars'
+
+// DOM-only components
+export { Transition, type TransitionProps } from './components/Transition'
+export {
+  TransitionGroup,
+  type TransitionGroupProps
+} from './components/TransitionGroup'
+
+// **Internal** DOM-only runtime directive helpers
+export {
+  vModelText,
+  vModelCheckbox,
+  vModelRadio,
+  vModelSelect,
+  vModelDynamic
+} from './directives/vModel'
+export { withModifiers, withKeys } from './directives/vOn'
+export { vShow } from './directives/vShow'
+
+import { initVModelForSSR } from './directives/vModel'
+import { initVShowForSSR } from './directives/vShow'
+
+let ssrDirectiveInitialized = false
+
+/**
+ * @internal
+ */
+export const initDirectivesForSSR = __SSR__
+  ? () => {
+    if (!ssrDirectiveInitialized) {
+      ssrDirectiveInitialized = true
+      initVModelForSSR()
+      initVShowForSSR()
+    }
+  }
+  : NOOP
+
+// re-export everything from core
+// h, Component, reactivity API, nextTick, flags & types
+export * from '@vue/runtime-core'
